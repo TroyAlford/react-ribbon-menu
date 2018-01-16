60b576b722eda2a0caa2580c6d467f69fa89fc79
Troy Alford <talford@vestmark.com>
2018-01-16 09:31:54 -0800

    Fix routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 import Account from './pages/Account';
 import Home from './pages/Home';
 import WelcomeToReact from './pages/WelcomeToReact';
 import './App.css';
 
+import menus from './data/Data.json';
+
 export default () => (
   <BrowserRouter>
-    <Route exact path="/" component={WelcomeToReact}>
-      <Route path="account" component={Account} />
-      <Route path="home" component={Home} />
-    </Route>
+    <Fragment>
+      <Route exact path="/" component={() => <WelcomeToReact menus={menus} />} />
+      <Route path="/account" component={() => <Account menus={menus} />} />
+      <Route path="/home" component={() => <Home menus={menus} />} />
+    </Fragment>
   </BrowserRouter>
 )
